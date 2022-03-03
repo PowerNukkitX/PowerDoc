b97383f9d8b465178da054f563bbda57427377a5
超神的冰凉 <2801621420@qq.com>
2022-03-03 22:57:31 +0800

    完成目录

--- src/main/java/cn/powernukkitx/powerdoc/render/HtmlCatalogueStep.java
+++ src/main/java/cn/powernukkitx/powerdoc/render/HtmlCatalogueStep.java
@@ -1,23 +1,18 @@
 package cn.powernukkitx.powerdoc.render;
 
-import cn.powernukkitx.powerdoc.Book;
 import cn.powernukkitx.powerdoc.config.Arg;
 import cn.powernukkitx.powerdoc.config.BookConfig;
 import cn.powernukkitx.powerdoc.config.Exposed;
 import cn.powernukkitx.powerdoc.config.NullableArg;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.WeakHashMap;
 import java.util.regex.Pattern;
 
 import static cn.powernukkitx.powerdoc.utils.StringUtils.beforeLast;
 
 public class HtmlCatalogueStep implements Step {
-    public static final Map<Book, String> cache = new WeakHashMap<>();
-
     protected final String navCssClass;
     protected final String divCssClass;
     protected final String listDivCssClass;
@@ -25,6 +20,7 @@ public class HtmlCatalogueStep implements Step {
     protected final String liCssClass;
     protected final String aCssClass;
     protected final String pCssClass;
+    protected final String strongCssClass;
 
     private final String navId;
 
@@ -38,6 +34,7 @@ public class HtmlCatalogueStep implements Step {
         this.liCssClass = cssClass.getOrDefault("li", " ");
         this.aCssClass = cssClass.getOrDefault("a", " ");
         this.pCssClass = cssClass.getOrDefault("p", " ");
+        this.strongCssClass = cssClass.getOrDefault("strong", " ");
         this.navId = navId != null ? navId : "catalogue";
     }
 
@@ -54,21 +51,16 @@ public class HtmlCatalogueStep implements Step {
     @Override
     public void work(Document document) {
         var book = document.getBook();
-        if (cache.containsKey(book)) {
-            document.setVariable("html.catalogue", cache.get(book));
-        } else {
-            var html = """
-                    <nav id="%s" class="%s"><div class="%s">%s</div></nav>""";
-            final var pattern = Pattern.compile(book.getConfig().pages().filter());
-            final var bookFile = new File(book.getConfig().pages().path());
-            html = html.formatted(navId, navCssClass, listDivCssClass, makeCatalogue(book.getConfig(),
-                    pattern, bookFile, document.getSource()));
-            document.setVariable("html.catalogue", html);
-            cache.put(book, html);
-        }
+        var html = """
+                <nav id="%s" class="%s"><div class="%s">%s</div></nav>""";
+        final var pattern = Pattern.compile(book.getConfig().pages().filter());
+        final var bookDir = new File(book.getConfig().pages().path());
+        html = html.formatted(navId, navCssClass, listDivCssClass, makeCatalogue(book.getConfig(),
+                pattern, bookDir, document.getSource().toFile(), 0));
+        document.setVariable("html.catalogue", html);
     }
 
-    private String makeCatalogue(BookConfig config, Pattern pageFileFilterPattern, File dir, Path currentPath) {
+    private String makeCatalogue(BookConfig config, Pattern pageFileFilterPattern, File dir, File workingFor, int depth) {
         var out = new StringBuilder();
         if (!dir.exists() || !dir.isDirectory()) {
             return "";
@@ -79,14 +71,19 @@ public class HtmlCatalogueStep implements Step {
         }
         for (final var each : pageFiles) {
             if (each.isFile()) {
-                out.append("""
-                        <li class="%s"><a class="%s" href="%s">%s</a></li>
-                        """.formatted(liCssClass, aCssClass,
-                        beforeLast(currentPath.relativize(each.toPath()).toString(), ".") + ".html",
-                        beforeLast(each.getName(), ".")));
+                final var href = relativeWebPath(workingFor, each);
+                if ("#".equals(href)) {
+                    out.append("""
+                            <li class="%s"><strong class="%s">%s</strong></li>
+                            """.formatted(liCssClass, strongCssClass, beforeLast(each.getName(), ".")));
+                } else {
+                    out.append("""
+                            <li class="%s"><a class="%s" href="%s">%s</a></li>
+                            """.formatted(liCssClass, aCssClass, beforeLast(href, ".") + ".html", beforeLast(each.getName(), ".")));
+                }
             } else if (each.isDirectory()) {
                 if (config.pages().recursion()) {
-                    out.append(makeCatalogue(config, pageFileFilterPattern, each, currentPath));
+                    out.append(makeCatalogue(config, pageFileFilterPattern, each, workingFor, depth + 1));
                 }
             }
         }
@@ -96,5 +93,24 @@ public class HtmlCatalogueStep implements Step {
                 %s
                 </ol>
                 """.formatted(olCssClass, pCssClass, dir.getName(), out.toString());
+    }
+
+    /**
+     * target相对于source的路径
+     *
+     * @param source File
+     * @param target File
+     * @return target相对于source的路径
+     */
+    private static String relativeWebPath(File source, File target) {
+        if(source.equals(target)) {
+            return "#";
+        }
+        final var sourceDir = source.isDirectory() ? source : source.getParentFile();
+        final var targetDir = target.isDirectory() ? target : target.getParentFile();
+        if (sourceDir.equals(targetDir)) {
+            return target.getName();
+        }
+        return (sourceDir.toPath().relativize(targetDir.toPath()) + "/" + target.getName()).replace("\\", "/");
     }
 }
