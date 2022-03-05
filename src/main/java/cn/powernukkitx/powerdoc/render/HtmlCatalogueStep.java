4f039cf21caecc2dbbfb858bc7d66df96353d4cd
超神的冰凉 <2801621420@qq.com>
2022-03-05 12:49:00 +0800

    支持目录文件排序

--- src/main/java/cn/powernukkitx/powerdoc/render/HtmlCatalogueStep.java
+++ src/main/java/cn/powernukkitx/powerdoc/render/HtmlCatalogueStep.java
@@ -5,15 +5,22 @@ import cn.powernukkitx.powerdoc.config.BookConfig;
 import cn.powernukkitx.powerdoc.config.Exposed;
 import cn.powernukkitx.powerdoc.config.NullableArg;
 import cn.powernukkitx.powerdoc.utils.FileUtils;
+import com.google.gson.JsonParser;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 import java.util.regex.Pattern;
 
 import static cn.powernukkitx.powerdoc.utils.StringUtils.beforeLast;
 
 public class HtmlCatalogueStep implements Step {
+    private static final WeakHashMap<File, File[]> childFileCache = new WeakHashMap<>();
+
     protected final String navCssClass;
     protected final String divCssClass;
     protected final String listDivCssClass;
@@ -66,11 +73,30 @@ public class HtmlCatalogueStep implements Step {
         if (!dir.exists() || !dir.isDirectory()) {
             return "";
         }
-        var pageFiles = dir.listFiles((dir1, name) ->
-                !name.contains(".") || pageFileFilterPattern.matcher(name).matches());
+        var pageFiles = childFileCache.get(dir);
+        if (pageFiles == null) {
+            pageFiles = dir.listFiles((dir1, name) ->
+                    !name.contains(".") || pageFileFilterPattern.matcher(name).matches());
+            final var cataloguePriorityFile = new File(dir, "cataloguePriority.json");
+            if (cataloguePriorityFile.exists() && pageFiles != null) {
+                try {
+                    final var priority = JsonParser.parseString(Files.readString(cataloguePriorityFile.toPath(), StandardCharsets.UTF_8)).getAsJsonObject();
+                    Arrays.sort(pageFiles, (a, b) -> {
+                        final var ta = priority.get(a.getName());
+                        final var pa = ta == null ? 0 : ta.getAsInt();
+                        final var tb = priority.get(b.getName());
+                        final var pb = tb == null ? 0 : tb.getAsInt();
+                        return pa - pb;
+                    });
+                } catch (Exception ignore) {
+
+                }
+            }
+        }
         if (pageFiles == null || pageFiles.length == 0) {
             return "";
         }
+        childFileCache.put(dir, pageFiles);
         for (final var each : pageFiles) {
             if (each.isFile()) {
                 final var href = relativeWebPath(workingFor, each);
@@ -105,7 +131,7 @@ public class HtmlCatalogueStep implements Step {
      * @return target相对于source的路径
      */
     private static String relativeWebPath(File source, File target) {
-        if(source.equals(target)) {
+        if (source.equals(target)) {
             return "#";
         }
         final var sourceDir = source.isDirectory() ? source : source.getParentFile();
